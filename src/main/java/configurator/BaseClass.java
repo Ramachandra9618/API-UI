124d9225a36e8ec07daa8cce044d83fc328a6d2e
rama chandra <ramachanndra2001@gmail.com>
2025-10-26 16:44:34 +0530

    08

--- src/main/java/configurator/BaseClass.java
+++ src/main/java/configurator/BaseClass.java
@@ -11,6 +11,7 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.*;
+import static utils.Utilities.addToTestDataFromProperties;
 
 public class BaseClass extends ApiService {
 
